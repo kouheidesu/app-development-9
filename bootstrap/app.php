6ab049034d4111fe75de84d6a3c46bee74699aab
gyarmex <gyarmex@gmail.com>
2025-12-13 11:11:32 +0900

    419エラー解消

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Session\TokenMismatchException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,14 +16,24 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR | \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST | \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT | \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO | \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function (TokenMismatchException $e, $request) {
+        $redirectToLogin = function () {
+            return redirect()->route('login')
+                ->with('error', 'セッションの有効期限が切れたため、再度ログインしてください。');
+        };
+
+        $exceptions->render(function (TokenMismatchException $e, $request) use ($redirectToLogin) {
             if ($request->expectsJson()) {
                 return response()->json([
                     'message' => 'セッションの有効期限が切れました。ページを再読み込みしてください。',
                 ], 419);
             }
 
-            return redirect()->route('login')
-                ->with('error', 'セッションの有効期限が切れたため、再度ログインしてください。');
+            return $redirectToLogin();
+        });
+
+        $exceptions->render(function (HttpException $e) use ($redirectToLogin) {
+            if ($e->getStatusCode() === 419) {
+                return $redirectToLogin();
+            }
         });
     })->create();
